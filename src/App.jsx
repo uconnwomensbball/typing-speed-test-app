46252baf66fd43ceb8a51505b5ec67aebcc36b72
uconnwomensbball <119827576+uconnwomensbball@users.noreply.github.com>
2026-04-25 21:36:13 -0700

    25 Apr 26 - feat: menu show/hide functionality; style: initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,46 @@ import { useState } from 'react'
 
 function App() {
 
+  const [isInitialScreen, setInitialScreen] = useState(true)
   return (
-    <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind 3</h1>
+    <>
+      <header className="flex flex-row justify-between items-center">
+        <img src="src\assets\images\logo-small.svg"/>
+        <div>
+        <img src="src\assets\images\icon-personal-best.svg"/>
+        <p>Best: 92 WPM</p>
+        </div>
+      </header>
 
-     
+      <main>
+        <nav>
+          <div>
+            <p>WPM:</p>
+            <p>0</p>
+          </div>
+          <div>
+            <p>Accuracy:</p>
+            <p>100%</p>
+          </div>
+          <div>
+            <p>Time:</p>
+            <p>0:60</p>
+          </div>
+        <select>
+          <option><button type="radio"></button>Hard</option>
+          <option><button type="radio"></button>Medium</option>
+          <option><button type="radio"></button>Easy</option>
+        </select>
+        <select>
+          <option><button type="radio"></button>Timed (60s)</option>
+          <option><button type="radio"></button>Passage</option>
+        </select>
+        </nav>
+        <button>Start Typing Test</button>
+        <p>Or click the text and start typing</p>
+      
+      </main>
+    </>
   )
 }
 
