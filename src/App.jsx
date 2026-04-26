718f2073114ee36637c08489db2420b74845b4f8
uconnwomensbball <119827576+uconnwomensbball@users.noreply.github.com>
2026-04-25 20:17:45 -0700

    25 Apr 26: init: inital commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+
+function App() {
+
+  return (
+    <h1 className="text-4xl font-bold text-blue-500">Hello Tailwind 3</h1>
+
+     
+  )
+}
+
+export default App
